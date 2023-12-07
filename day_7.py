b42425e4230f81133ff9ebd88866330bbdd442d8
Patrick Smadbeck <patsmad@gmail.com>
2023-12-07 07:29:22 -0500

    maybe try some functional today

--- day_7.py
+++ day_7.py
@@ -1,4 +1,5 @@
 import argparse
+from collections.abc import Callable
 from get_input import get_input
 import re
 
@@ -16,23 +17,25 @@ def format(raw_input: str) -> (list, dict[str, int]):
         bids[hand] = int(bid)
     return hands, bids
 
-card_value: dict[str, int] = {card: i + 1 for i, card in enumerate(
-    ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-)}
-card_value_joker: dict[str, int] = {card: i + 1 for i, card in enumerate(
-    ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
-)}
+card_order: list[str] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+card_order_joker: list[str] = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
 
-def hand_value(hand: str):
+def hand_value(hand: str) -> int:
     count: dict[str, int] = {}
     for card in hand:
         count[card] = count.get(card, 0) + 1
     hand_value: int = sum([count[card] for card in hand])
-    return hand_value, *[card_value.get(card, 0) for card in hand]
+    return hand_value
 
-def joker_hand_value(hand: str):
-    possible_hand_values = [hand_value(hand.replace('J', possible_card))[0] for possible_card in card_value_joker.keys()]
-    return max(possible_hand_values), *[card_value_joker.get(card, 0) for card in hand]
+def hand_value_joker(hand: str) -> int:
+    possible_hand_values = [hand_value(hand.replace('J', possible_card)) for possible_card in card_order_joker]
+    return max(possible_hand_values)
+
+def sort_fnc(card_order: list[str], hand_value_fnc: Callable[[str], int]) -> Callable[[str], tuple]:
+    card_dict: dict[str, int] = {card: i for i, card in enumerate(card_order)}
+    def hand_sort(hand: str) -> tuple:
+        return hand_value_fnc(hand), *[card_dict[card] for card in hand]
+    return hand_sort
 
 def run(test: bool) -> None:
     if not test:
@@ -50,13 +53,13 @@ def run(test: bool) -> None:
 
     # part 1
     part_1 = 0
-    for rank, hand in enumerate(sorted(hands, key=hand_value)):
+    for rank, hand in enumerate(sorted(hands, key=sort_fnc(card_order, hand_value))):
         part_1 += (rank + 1) * bids[hand]
     print(part_1)
 
     # part 2
     part_2 = 0
-    for rank, hand in enumerate(sorted(hands, key=joker_hand_value)):
+    for rank, hand in enumerate(sorted(hands, key=sort_fnc(card_order_joker, hand_value_joker))):
         part_2 += (rank + 1) * bids[hand]
     print(part_2)
 
